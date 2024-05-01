4c6d5799e1523ad2b00fefd6bb1c2855234baece
Huynhminhduc2k2 <hmduc20@clc.fitus.edu.vn>
2024-05-01 19:55:21 +0700

    Add API findTripByRoute

--- src/routes/v1/public.route.ts
+++ src/routes/v1/public.route.ts
@@ -1,7 +1,8 @@
-import { publicLog } from "../../controllers/anonymous.controller";
+import { publicLog, publicSearchTrip } from "../../controllers/anonymous.controller";
 import express from "express";
 const publicRoute = express.Router();
 
 publicRoute.get("/", publicLog);
+publicRoute.post("/findTrip", publicSearchTrip);
 
 export default publicRoute;
--- src/services/trip.service.ts
+++ src/services/trip.service.ts
@@ -9,16 +9,36 @@ import ApiError from "../utils/ApiError";
  */
 
 /**
- * Get Trip by id
- * @param {ObjectId} id
+ * Get Trip by route
+ * @param {Object} reqRoute
  * @returns {Promise<Trip>}
  */
-const getTripById = async (id) => {
-  return Trip.findById(id).lean();
+
+const getTripByRoute = async (reqRoute) => {
+  const { route, departureTime } = reqRoute; // Expected input format: '2024-05-01'
+
+  const query = { route, departureTime };
+
+  if (departureTime) {
+    const startOfDay = new Date(departureTime); // Start of the specified date
+    const endOfDay = new Date(startOfDay);          // Duplicate date object
+
+    // Adjust endOfDay to represent the very end of the specified date
+    endOfDay.setDate(endOfDay.getDate() + 1);      // Move to the next day
+    endOfDay.setMilliseconds(-1);                  // Set to the very end of the previous day
+
+    query.departureTime = {
+      $gte: startOfDay, // Greater than or equal to the start of the specified day
+      $lt: endOfDay,    // Less than the start of the following day
+    };
+  }
+
+  return await Trip.find(query).lean();
 };
+
 
 const getAllTrips = async()=>{
     return Trip.find();
 }
 
-export {getAllTrips};
+export {getAllTrips, getTripByRoute};
