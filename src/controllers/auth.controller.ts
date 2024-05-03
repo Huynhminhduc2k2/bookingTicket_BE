df2c8052b5827c44857839fc7bc1104b327ececd
ducanng <ducan172002@gmail.com>
2024-05-04 02:44:20 +0700

    feat: change sign in, sign up by phone and add verify OTP

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import * as httpStatus from "http-status";
 import catchAsync from "../utils/catchAsync";
-import { createUser, getUserByEmail } from "../services/user.service";
+import { createUser, getUserByPhone, isOTPMatch } from "../services/user.service";
 import { generateAuthTokens, removeToken } from "../services/token.service";
 
 const register = catchAsync(async (req, res) => {
@@ -13,9 +13,9 @@ const register = catchAsync(async (req, res) => {
 });
 
 const login = catchAsync(async (req, res) => {
-  const { email, password } = req.body;
+  const { phone, password } = req.body;
 
-  const user = await getUserByEmail(email);
+  const user = await getUserByPhone(phone);
 
   if (!user) {
     return res.status(httpStatus.UNAUTHORIZED).send({
@@ -43,4 +43,25 @@ const logout = catchAsync(async (req, res) => {
   });
 });
 
-export { register, login, logout };
+const verifyOTP = catchAsync(async (req, res) => {
+  const { phone, otp } = req.body;
+  const user = await getUserByPhone(phone);
+
+  if (!user) {
+    return res.status(httpStatus.UNAUTHORIZED).send({
+      message: "Không tìm thấy số điện thoại này",
+    });
+  }
+
+  const ok = await isOTPMatch(phone, otp);
+  if (!ok) {
+    return res.status(httpStatus.NOT_FOUND).send({
+      message: "Mã OTP không đúng",
+    });
+  }
+
+  const token = await generateAuthTokens(user);
+  res.status(httpStatus.OK).send({ message: "login successful", user, token });
+});
+
+export { register, login, logout, verifyOTP };
